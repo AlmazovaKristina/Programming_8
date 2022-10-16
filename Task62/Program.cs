473d9a0b1a0bd6ee946a135f779d873302940aef
Almazova Kristina <cristinaalmaz1702@gmail.com>
2022-10-16 22:17:39 +0300

    Task62

---
+++ Task62/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите размер стороны квадратной матрицы: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int matrix = new int[size, size];
+PopulateSpiral(matrix);
+
+void PopulateSpiral(int[,] matrix)
+{
+    int num = 1;
+    int max = matrix.GetLength(0) * matrix.GetLength(1);
+    int step = matrix.GetLength(0);
+    int turns = 0;
+    while (num <= max)
+    {
+        for (int two = 0; two < 2; two++)
+        {
+            for (int i = 0; i < matrix.GetLength(0) - turns; i++)
+            {
+                
+            }
+        }
+    }
+}
